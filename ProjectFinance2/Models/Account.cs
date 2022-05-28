593ef34d876bc6240e98088b303419d2ec56a11f
Guilherme Gimenes <ippad01@gmail.com>
2022-05-28 13:35:15 -0300

    Adicionar arquivos de projeto.

---
+++ ProjectFinance2/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFinance.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        public string Name { get; set; }
+
+        public int AccountNumber { get; set; }
+
+        public float CurrentBalance { get; set; }
+
+    }
+}
---
+++ ProjectFinance2/Models/FinancialTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFinance.Models
+{
+    public class FinancialTransaction
+    {
+        public int TransactionId { get; set; }
+
+        public int DestinationAccount { get; set; }
+
+        //Will be an optional field on screen
+        public string? Description { get; set; }
+
+        public float Value { get; set; }
+
+        //Will use 1 and 2 to tell if there is a credit or a debit in that account
+        public int Nature { get; set; }
+    }
+}
